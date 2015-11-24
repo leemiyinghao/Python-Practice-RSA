f0b4fbbd7d4b71a07556d8ff97d9b604439be93e
catLee <leemiyinghao@gmail.com>
2015-11-24 23:06:19 +0800

    modify to a more OO style

--- rsa.py
+++ rsa.py
@@ -16,32 +16,42 @@ def genPrime(start, end):
       num = random.randint(start, end)
       if isPrime(num):
          return num
+class RSA:
+   def __init__(self):
+      self.genKey()
+   def genKey(self):
+      #p & q are two different prime
+      q = p = genPrime(50, 100)
+      while p == q:
+         q = genPrime(50, 100)
+      n = p*q
+      r = (p-1)*(q-1)
 
-#p & q are two different prime
-q = p = genPrime(50, 100)
-while p == q:
-   q = genPrime(50, 100)
-n = p*q
-r = (p-1)*(q-1)
+      #find an e is a coprime to r
+      e = 0
+      for num in range(2,r):
+         if isPrime(num) and r%num != 0:
+            e = num
+            break
 
-#find an e is a coprime to r
-e = 0
-for num in range(2,r):
-   if isPrime(num) and r%num != 0:
-      e = num
-      break
-
-#find a modular multiplicative inverse of e
-d = 0
-for num in range(2,r):
-   if (e*num)%r == 1:
-      d = num
+      #find a modular multiplicative inverse of e
+      d = 0
+      for num in range(2,r):
+         if (e*num)%r == 1:
+            d = num
+      #save public and private key
+      self.publicKey = {'N':n,'e':e}
+      self.privateKey = {'N':n, 'd':d}
+   def encode(self, plainNumber):
+      return plainNumber**self.publicKey['e'] % self.publicKey['N']
+   def decode(self, cipherNumber):
+      return cipherNumber**self.privateKey['d'] % self.publicKey['N']
 
 #let's encrypt
+rsa = RSA() #init a RSA object
 plainNumber = 42 #the answer to the meaning of life, the universe, and everything
-cipherNumber = plainNumber**e%n
-print("p=%d q=%d N=%d r=%d e=%d d=%d"%(p,q,n,r,e,d))
+cipherNumber = rsa.encode(plainNumber) #calling RSA object to encode
+replainNumber = rsa.decode(cipherNumber) #calling RSA object to decode
 print("plainNumber:", plainNumber)
 print("cipherNumber:", cipherNumber)
-replainNumber = cipherNumber**d%n
 print("replainNumber:",int(replainNumber))
